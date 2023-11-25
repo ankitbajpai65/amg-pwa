b175d576300f031bdaf1874b97e51c7a38cb6582
AurobindoGupta <aurogpt10@gmail.com>
2023-11-25 19:53:11 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Login from "./containers/Login";
+
+function App() {
+  return (
+    <div className="border-box m-2 h-screen flex items-center">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      mobile: "370px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
