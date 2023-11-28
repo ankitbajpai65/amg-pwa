c53db286a2fd99df297c5a1e144952527518d985
AurobindoGupta <aurogpt10@gmail.com>
2023-11-28 15:49:04 +0530

    logout functionality

--- src/components/appComponents/Header.tsx
+++ src/components/appComponents/Header.tsx
@@ -1,7 +1,25 @@
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { FaRegUser } from "react-icons/fa";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useDispatch } from "react-redux";
+import { setIsLoggedIn } from "@/containers/login/loginSlice";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/");
+    dispatch(setIsLoggedIn(false));
+  };
   return (
     <>
       <div className="bg-red-600 rounded text-white font-semibold pt-1 mb-2 w-full text-center">
@@ -9,7 +27,18 @@ const Header = () => {
           <h2 className="text-2xl">AMG</h2>
           <div className="flex w-1/6 justify-around p-1">
             <IoIosNotificationsOutline size={25} />
-            <FaRegUser size={25} />
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <FaRegUser size={25} />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Menu</DropdownMenuLabel>
+                <DropdownMenuSeparator></DropdownMenuSeparator>
+                <DropdownMenuItem onClick={() => handleLogout()}>
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
         <p>Home</p>
