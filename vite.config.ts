96a515d722109f73b2190cdb90fef60f4d22e1fc
AurobindoGupta <aurogpt10@gmail.com>
2023-12-22 22:42:30 +0530

    remove viteplugin pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,71 +1,7 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
-const manifestForPlugin: Partial<VitePWAOptions> = {
-  registerType: "prompt",
-  includeAssets: [
-    "manifest-icon-192.maskable.png",
-    "favicon.ico",
-    "apple-touch-icon.png",
-  ],
-  manifest: {
-    name: "AMG-PWA",
-    short_name: "PWA",
-    description: "A mobile app for AMG Datapartners.",
-    icons: [
-      {
-        src: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/logo/manifest-icon-512.maskable.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
-    ],
-    theme_color: "#171717",
-    background_color: "#f0e7db",
-    display: "standalone",
-    orientation: "portrait",
-  },
-};
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [
-    react(),
-    VitePWA({
-      workbox: {
-        globPatterns: ["**/*"],
-      },
-      includeAssets: ["**/*"],
-      manifest: manifestForPlugin,
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
 });
